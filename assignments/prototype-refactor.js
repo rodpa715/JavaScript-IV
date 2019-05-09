b447e351a79e71871c6711e9bcc4d016f1e263bb
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-09 13:33:22 +0200

    Refactor code into class constructor with stretch goal

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,21 +7,6 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
-// function GameObject(attributes){
-//     this.createdAt = attributes.createdAt;
-//     this.name = attributes.name;
-//     this.dimensions = attributes.dimensions;
-//   }
-//   GameObject.prototype.destroy = function(){
-//     return `${this.name} was removed from the game.`
-//   }
     class GameObject{
         constructor(attributes){
             this.createdAt = attributes.createdAt;
@@ -29,51 +14,31 @@ Prototype Refactor
             this.dimensions = attributes.dimensions;
         }
         destroy(){
-            return `${this.name} was removed from the game.`
+            return `${this.name} was removed from the game.`;
         }
     }
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
-function CharacterStats(attributes){
-  GameObject.call(this, attributes)
-  this.healthPoints = attributes.healthPoints;
-}
-CharacterStats.prototype = Object.create(GameObject.prototype)
-CharacterStats.prototype.takeDamage = function(){
-  return `${this.name}took damage.`
+
+class CharacterStats extends GameObject{
+    constructor(attributes){
+        super(attributes);
+        this.healthPoints = attributes.healthPoints;
+    }
+    takeDamage(){
+        return `${this.name}took damage.`
+    }
 }
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
- function Humanoid(attributes){
-   CharacterStats.call(this,attributes);
-   this.team = attributes.team;
-   this.weapons = attributes.weapons;
-   this.language = attributes.language;
- }
- Humanoid.prototype = Object.create(CharacterStats.prototype)
- Humanoid.prototype.greet = function(){
-  return `${this.name}offers a greeting in ${this.language}`
- }
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
-
-// Test you work by un-commenting these 3 objects and the list of console logs below:
-
+ class Humanoid extends CharacterStats{
+    constructor(attributes){
+        super(attributes);
+        this.team = attributes.team;
+        this.weapons = attributes.weapons;
+        this.language = attributes.language;
+      }
+      greet(){
+        return `${this.name}offers a greeting in ${this.language}`
+      }
+    }
 
   const mage = new Humanoid({
     createdAt: new Date(),
@@ -134,3 +99,54 @@ CharacterStats.prototype.takeDamage = function(){
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+// Stretch Goal
+
+  class Figher extends Humanoid{
+    constructor(attributes){
+        super(attributes)
+    }
+    dealDamage(object, dmg = 10){
+        object.healthPoints -= dmg
+        if (object.healthPoints <= 0){
+            console.log(object.destroy())
+        }else{
+        console.log(`${this.name} just dealt ${dmg} DMG to ${object.name}, he now has ${object.healthPoints} HP`)
+        }
+    }
+  }
+
+  const kaido = new Figher({ //villain
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 3,
+      height: 6,
+    },
+    healthPoints: 300,
+    name: 'Kaido',
+    team: 'Beasts Pirates',
+    weapons: [
+      'Giant Sword',
+      "Conqueror's Haki",
+    ],
+    language: 'Japanese',
+  });
+
+  
+  const luffy = new Figher({ //hero
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 1,
+      height: 2,
+    },
+    healthPoints: 150,
+    name: 'Monkey D. Luffy',
+    team: 'Straw Hats Pirates',
+    weapons: [
+      'Gomu Gomu no mi',
+      "Conqueror's Haki",
+    ],
+    language: 'Japanese',
+  });
